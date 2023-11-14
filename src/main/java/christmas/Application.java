835b3d7fa7d1398fef33715329d2f18fcbee0726
san <soh7174@gmail.com>
2023-11-15 02:33:14 +0900

    feat : main() 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,10 @@
 package christmas;
 
+import christmas.controller.ChristmasPromotionApp;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ChristmasPromotionApp app = new ChristmasPromotionApp();
+        app.run();
     }
 }
