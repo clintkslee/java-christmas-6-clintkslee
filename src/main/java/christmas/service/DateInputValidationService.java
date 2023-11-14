156684e12db4f91b0de423a5c2d3706b7f8707d9
san <soh7174@gmail.com>
2023-11-15 08:27:35 +0900

    feat : 입력 받은 날짜 유효한지 검사하는 DateInputValidationService 구현

--- src/main/java/christmas/service/DateInputValidationService.java
+++ src/main/java/christmas/service/DateInputValidationService.java
@@ -1,4 +1,20 @@
 package christmas.service;
 
+import static christmas.constants.Constants.INPUT_DATE_ERROR_MESSAGE;
+
 public class DateInputValidationService {
+    public boolean isNumber(String dateInput) {
+        try {
+            Integer.parseInt(dateInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INPUT_DATE_ERROR_MESSAGE);
+        }
+        return true;
+    }
+    public boolean isNumberBetween1to31(String dateInput) {
+        if(Integer.parseInt(dateInput)<0 || 31<Integer.parseInt(dateInput)) {
+            throw new IllegalArgumentException(INPUT_DATE_ERROR_MESSAGE);
+        }
+        return true;
+    }
 }
