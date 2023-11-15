c37daab65c64c9b3f45b98800981ddf47a063c4c
san <soh7174@gmail.com>
2023-11-15 16:47:52 +0900

    feat : 출력 담당하는 OutputView 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,61 @@
 package christmas.view;
 
+import christmas.model.Customer;
+
+import static christmas.constants.Constants.*;
+
 public class OutputView {
-    public void printMenu() {
-        System.out.println("<주문 메뉴>");
-        // ...
+    private Customer customer;
+    public OutputView(Customer customer) {
+        this.customer = customer;
     }
-    // ...
+    public void printResult() {
+        System.out.println(ORDER_MENU_LABEL);
+        for(int i=0; i<customer.getMenu().length; i+=2) {
+            System.out.println(customer.getMenu()[i]+" "+customer.getMenu()[i+1]+EA);
+        }
+        System.out.println();
+
+        System.out.println(TOTAL_BEFORE_DISCOUNT_LABEL);
+        System.out.println(customer.getTotalBeforeDiscount()+EA);
+        System.out.println();
+
+        System.out.println(GIFT_MENU_LABEL);
+        if(customer.isGiftMenu()) {
+            System.out.println("샴페인 1개");
+        }
+        else
+            System.out.println(NONE);
+        System.out.println();
+
+        System.out.println(BENEFIT_DETAILS_LABEL);
+        if(customer.getChristmasDiscount()!=0) {
+            System.out.println(CHRISTMAS_DISCOUNT_PREFIX+"-"+customer.getChristmasDiscount()+WON);
+        }
+        if(customer.getWeekdayDiscount()!=0) {
+            System.out.println(WEEKDAY_DISCOUNT_PREFIX+"-"+customer.getWeekdayDiscount()+WON);
+        }
+        if(customer.getWeekendDiscount()!=0) {
+            System.out.println(WEEKEND_DISCOUNT_PREFIX+"-"+customer.getWeekendDiscount()+WON);
+        }
+        if(customer.isSpecialDiscount()) {
+            System.out.println(SPECIAL_DISCOUNT_PREFIX+"-1000"+WON);
+        }
+        if(customer.isGiftMenu()) {
+            System.out.println(GIFT_EVENT_PREFIX+"-25000"+WON);
+        }
+        // 없음에 대한 예외처리
+        System.out.println();
+
+        System.out.println(TOTAL_DISCOUNT_LABEL);
+        System.out.println("-"+customer.getTotalDiscount()+WON);
+        System.out.println();
+
+        System.out.println(TOTAL_AFTER_DISCOUNT_LABEL);
+        System.out.println(customer.getTotalBeforeDiscount()-customer.getTotalDiscount()+WON);
+        System.out.println();
+
+        System.out.println(BADGE_LABEL);
+        System.out.println(customer.getBadge());
+    }
 }
